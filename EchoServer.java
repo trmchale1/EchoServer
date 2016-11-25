6cbe05a4e3bff9331f359a34823a0c447280b6b3
Tim <trmchale@gmail.com>
2016-11-25 13:40:14 -0500

    Done

---
+++ EchoServer.java
@@ -0,0 +1,37 @@
+import java.net.*;
+import java.io.*;
+ 
+public class EchoServer {
+    public static void main(String[] args) throws IOException {
+         if (args.length != 1) {
+            System.err.println("Usage: java EchoServer <port number>");
+            System.exit(1);
+        }
+         
+        int portNumber = Integer.parseInt(args[0]);
+         try (
+            ServerSocket serverSocket =
+                new ServerSocket(Integer.parseInt(args[0]));
+
+	// ServerSocket method accept() returns a Socket
+
+            Socket clientSocket = serverSocket.accept();
+	// printwriter can print nonencoded byte streams like strings     
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+	// Buffered Reader accepts an input stream, which accepts the sockets input stream                   
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        ){
+            String inputLine;
+	// Buffered Reader accepts the sockets input stream and we print it with PrintWriter
+	    
+            while ((inputLine = in.readLine()) != null) {
+                out.println(inputLine);
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
+}
